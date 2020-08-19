7ef722f883483bcce1262efffb3a724cfadc1d55
kotaMiya <kota.miyamoto18@gmail.com>
2020-08-19 10:28:00 +0900

    add home initial props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
-import React, { FC } from 'react';
+import { NextPage } from 'next'
 
-const Home: FC = () => {
+interface Props {
+  data: {
+    title: string;
+    description: string;
+  }
+}
+
+const Home: NextPage<Props> = props => {
+  console.log(props);
   return (
     <div>
-      hello
+      <p>{props.data.title}</p>
+      <p>{props.data.description}</p>
     </div>
   )
+}
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://localhost:8000/todo');
+  const json = await res.json();
+  return {
+    data: {
+      title: json.title,
+      description: json.descritpion
+    }
+  };
 }
 
 export default Home;
