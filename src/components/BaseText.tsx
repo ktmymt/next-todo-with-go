937195bb1ad62fcf329ed78fb4cdc4884883d3c0
kotaMiya <kota.miyamoto18@gmail.com>
2020-09-06 18:26:05 +0900

    add base text component

---
+++ src/components/BaseText.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+type Props = {
+  text: string;
+  className: string;
+}
+
+const BaseText: FC<Props> = props => {
+  return (
+    <p className={props.className}>
+      {props.text}
+    </p>
+  )
+}
+
+export default BaseText;
