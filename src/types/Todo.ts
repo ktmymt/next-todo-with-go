4342b92b6cd1e5c89b022b2e827323ee2008ed8e
ktmymt <kota.miyamoto18@gmail.com>
2021-05-16 01:07:02 +0900

    add todo interface and dummy data

---
+++ src/types/Todo.ts
@@ -0,0 +1,19 @@
+export interface ITodo {
+  id: number
+  title: string
+}
+
+export const todos: ITodo[] = [
+  {
+    id: 1,
+    title: "todo 1",
+  },
+  {
+    id: 2,
+    title: "todo 2",
+  },
+  {
+    id: 3,
+    title: "todo 3",
+  },
+]
