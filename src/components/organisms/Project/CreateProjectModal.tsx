5d61c76abd08a436ed4ab49b57363d4adad7f97d
ktmymt <kota.miyamoto18@gmail.com>
2021-06-21 23:39:11 +0900

    fix

--- src/components/organisms/Project/CreateProjectModal.tsx
+++ src/components/organisms/Project/CreateProjectModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react"
+import { FC, useState } from "react"
 import Modal from "react-modal"
 import { BaseButton, BaseInput, BaseTextArea, BaseText } from "../../atoms"
 import { Colors } from "../../../styles/colors"
