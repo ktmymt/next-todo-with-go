834be34d77c325aa4162c36234e52e4b9c77f2bc
ktmymt <kota.miyamoto18@gmail.com>
2021-07-30 00:34:49 +0900

    fix create modal design

--- src/components/organisms/Project/CreateProjectModal.tsx
+++ src/components/organisms/Project/CreateProjectModal.tsx
@@ -2,39 +2,11 @@ import { FC, Fragment, useState } from "react"
 import Modal from "react-modal"
 import { BaseButton, BaseInput, BaseTextArea, BaseText } from "../../atoms"
 import { Colors } from "../../../styles/colors"
-import { css } from "@emotion/react"
+import { css, ClassNames } from "@emotion/react"
 import { useProjectContext } from "../../../contexts/ProjectContext"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCheck } from "@fortawesome/free-solid-svg-icons"
 import { useUserContext } from "../../../contexts/UserContext"
-
-// for modal
-const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    backgroundColor: "rgba(0,0,0,0.3)",
-  },
-
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    width: "400px",
-    height: "600px",
-    backgroundColor: "#3D3D40",
-    borderStyle: "none",
-    borderRadius: "20px",
-    color: "#fff",
-    transform: "translate(-50%, -50%)",
-    transitionProperty: "background-color, width, height",
-    transitionDuration: "500ms",
-    transitionTimingFunction: "ease-in-out",
-  },
-}
 
 const inputAreaStyle = css`
   width: 100%;
@@ -132,55 +104,126 @@ const CreateProjectModal: FC = () => {
   return (
     <Fragment>
       <BaseButton text="+" bgColor={Colors.purple} onClickButton={onClickModalOpen} />
-      <Modal isOpen={modalIsOpen} onRequestClose={onClickModalClose} style={customStyles}>
-        <div css={inputAreaStyle}>
-          <h2>Create a Workspace</h2>
-          <BaseInput
-            type="text"
-            placeholder="Project Name"
-            hasLabel={true}
-            onChangeText={setProjectTitle}
-          />
-          <span>{projectNameError}</span>
-          <div css={colorButtonContainerStyle}>
-            <BaseText text="Color" size="1rem" color={Colors.lightGray} />
-            {Object.entries(Colors.projectCards).map((color, index) => {
-              return (
-                <span
-                  key={index}
-                  css={
-                    projectColor == color[0]
-                      ? selectedColorButtonStyle(color[1])
-                      : colorButtonStyle(color[1])
-                  }
-                  onClick={() => setProjectColor(color[0])}
-                >
-                  {projectColor == color[0] ? (
-                    <FontAwesomeIcon icon={faCheck} css={checkIconStyle} />
-                  ) : (
-                    ""
-                  )}
-                </span>
-              )
-            })}
-          </div>
-          <span>{projectColorError}</span>
-          <BaseTextArea
-            placeholder="Write some description for your workspace"
-            onChangeText={setProjectDescription}
-          />
-        </div>
-        <span>{projectDescriptionError}</span>
-        <div css={buttonStyle}>
-          <BaseButton text="Close" bgColor={Colors.offWhite} onClickButton={onClickModalClose} />
-          <BaseButton
-            text="Create"
-            bgColor={Colors.purple}
-            textColor={Colors.white}
-            onClickButton={onClickCreate}
-          />
-        </div>
-      </Modal>
+      <ClassNames>
+        {({ css, cx }) => (
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={onClickModalClose}
+            overlayClassName={{
+              base: "overlay-base",
+              afterOpen: "overlay-after",
+              beforeClose: "overlay-before",
+            }}
+            className={{
+              base: "content-base",
+              afterOpen: "content-after",
+              beforeClose: "content-before",
+            }}
+            closeTimeoutMS={500}
+            portalClassName={css`
+              .overlay-base {
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                right: 0;
+                left: 0;
+                background-color: "rgba(0,0,0,0.3)";
+                opacity: 0;
+                transition-duration: 200ms;
+                transition-timing-function: ease-in-out;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+
+              .overlay-after {
+                background-color: rgba(0, 0, 0, 0.8);
+                opacity: 1;
+              }
+
+              .overlay-before {
+                opacity: 0;
+              }
+
+              .content-base {
+                position: relative;
+                top: auto;
+                left: auto;
+                right: auto;
+                bottom: auto;
+                overflow: auto;
+                transition-duration: 100ms;
+                transition-timing-function: ease-in-out;
+              }
+
+              .content-after {
+                color: #fff;
+
+                @media screen and (max-width: 1320px) {
+                  width: 35%;
+                }
+
+                width: 20%;
+                padding: 20px;
+                border-radius: 20px;
+                background-color: #3d3d40;
+              }
+            `}
+          >
+            <div css={inputAreaStyle}>
+              <h2>Create a Workspace</h2>
+              <BaseInput
+                type="text"
+                placeholder="Project Name"
+                hasLabel={true}
+                onChangeText={setProjectTitle}
+              />
+              <span>{projectNameError}</span>
+              <div css={colorButtonContainerStyle}>
+                <BaseText text="Color" size="1rem" color={Colors.lightGray} />
+                {Object.entries(Colors.projectCards).map((color, index) => {
+                  return (
+                    <span
+                      key={index}
+                      css={
+                        projectColor == color[0]
+                          ? selectedColorButtonStyle(color[1])
+                          : colorButtonStyle(color[1])
+                      }
+                      onClick={() => setProjectColor(color[0])}
+                    >
+                      {projectColor == color[0] ? (
+                        <FontAwesomeIcon icon={faCheck} css={checkIconStyle} />
+                      ) : (
+                        ""
+                      )}
+                    </span>
+                  )
+                })}
+              </div>
+              <span>{projectColorError}</span>
+              <BaseTextArea
+                placeholder="Write some description for your workspace"
+                onChangeText={setProjectDescription}
+              />
+            </div>
+            <span>{projectDescriptionError}</span>
+            <div css={buttonStyle}>
+              <BaseButton
+                text="Close"
+                bgColor={Colors.offWhite}
+                onClickButton={onClickModalClose}
+              />
+              <BaseButton
+                text="Create"
+                bgColor={Colors.purple}
+                textColor={Colors.white}
+                onClickButton={onClickCreate}
+              />
+            </div>
+          </Modal>
+        )}
+      </ClassNames>
     </Fragment>
   )
 }
