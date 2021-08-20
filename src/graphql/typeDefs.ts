17b3806fb45abbe1746a04aabb3db81be3302d25
ktmymt <kota.miyamoto18@gmail.com>
2021-08-21 01:48:15 +0900

    split files

---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,39 @@
+import { gql } from "apollo-server-micro"
+
+export const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    image: String!
+  }
+
+  type Project {
+    id: ID!
+    name: String!
+    description: String
+    color: String!
+    userId: Int!
+  }
+
+  type Todo {
+    id: ID!
+    title: String!
+    status: String!
+    isDone: Boolean!
+    schedule: Int!
+    projectId: Int!
+  }
+
+  type Query {
+    getUsers: [User]
+    getProjects: [Project]
+    getTodos: [Todo]
+  }
+
+  type Mutation {
+    addUser(name: String, email: String, image: String): User
+    addProject(name: String, description: String, color: String, userId: Int): Project
+    addTodo(title: String, status: String, isDone: Boolean, schedule: Int, projectId: Int): Todo
+  }
+`
